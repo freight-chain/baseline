6e2e4d036c17239eb0139e82ef8bd569103c3e05
Restyled.io <commits@restyled.io>
2020-08-11 22:35:10 +0000

    Restyled by clang-format

--- examples/radish34/api/src/graphql/resolvers/serverSettings.js
+++ examples/radish34/api/src/graphql/resolvers/serverSettings.js
@@ -1,48 +1,57 @@
+import {logger} from 'radish34-logger';
+
 import healthcheck from '../../install/healthcheck';
-import { pubsub } from '../subscriptions';
-import { setRPCProvider, setContractAddress, getServerSettings } from '../../utils/serverSettings';
-import { createWalletFromMnemonic } from '../../utils/wallet';
-import { logger } from 'radish34-logger';
+import {
+  getServerSettings,
+  setContractAddress,
+  setRPCProvider
+} from '../../utils/serverSettings';
+import {createWalletFromMnemonic} from '../../utils/wallet';
+import {pubsub} from '../subscriptions';
 
 const SERVER_SETTINGS_UPDATE = 'SERVER_SETTINGS_UPDATE';
 
 export default {
   Query: {
     async getServerSettings() {
-      const { rpcProvider, organization, addresses } = await getServerSettings();
+      const {rpcProvider, organization, addresses} = await getServerSettings();
       logger.info(`
         Getting server settings:
         rpcProvider: ${rpcProvider}
         organization: ${organization}
         addresses: ${addresses}
-      `, { service: 'API' });
-      // TODO: Edit ServerSettings graphQL schema to be a nested object with 'organizations' and 'addresses' sub-objects, to align with the radosh-api backend.
+      `,
+                  {service : 'API'});
+      // TODO: Edit ServerSettings graphQL schema to be a nested object with
+      // 'organizations' and 'addresses' sub-objects, to align with the
+      // radosh-api backend.
       const flattenedSettings = {
         rpcProvider,
-        organizationName: organization.name,
-        organizationRole: organization.role,
-        organizationWhisperKey: organization.messagingKey,
-        organizationAddress: organization.address,
-        globalRegistryAddress: addresses.ERC1820Registry,
-        orgRegistryAddress: addresses.OrgRegistry,
+        organizationName : organization.name,
+        organizationRole : organization.role,
+        organizationWhisperKey : organization.messagingKey,
+        organizationAddress : organization.address,
+        globalRegistryAddress : addresses.ERC1820Registry,
+        orgRegistryAddress : addresses.OrgRegistry,
       };
       return flattenedSettings;
     },
   },
   Mutation: {
-    setRPCProvider: async (_parent, args) => {
-      await setRPCProvider(args.uri);
-      healthcheck();
-      const settings = await getServerSettings();
-      return settings;
-    },
-    setOrgRegistryAddress: async (_parent, args) => {
+    setRPCProvider:
+        async (_parent, args) => {
+          await setRPCProvider(args.uri);
+          healthcheck();
+          const settings = await getServerSettings();
+          return settings;
+        },
+    setOrgRegistryAddress : async (_parent, args) => {
       await setContractAddress('OrgRegistry', args.orgRegistryAddress);
       healthcheck();
       const settings = await getServerSettings();
       return settings;
     },
-    setWalletFromMnemonic: async (_parent, args) => {
+    setWalletFromMnemonic : async (_parent, args) => {
       await createWalletFromMnemonic(args.mnemonic, args.path);
       healthcheck();
       const settings = await getServerSettings();
@@ -51,9 +60,7 @@ export default {
   },
   Subscription: {
     serverSettingsUpdate: {
-      subscribe: () => {
-        return pubsub.asyncIterator(SERVER_SETTINGS_UPDATE);
-      },
+      subscribe: () => { return pubsub.asyncIterator(SERVER_SETTINGS_UPDATE);},
     },
   },
 };
--- examples/radish34/api/src/services/notice.js
+++ examples/radish34/api/src/services/notice.js
@@ -1,51 +1,42 @@
+import {logger} from 'radish34-logger';
+
 import db from '../db';
-import { pubsub } from '../graphql/subscriptions';
-import { logger } from 'radish34-logger';
+import {pubsub} from '../graphql/subscriptions';
 
 export const getNoticeById = async id => {
-  const notice = await db.collection('notices').findOne({ _id: id });
+  const notice = await db.collection('notices').findOne({_id : id});
   return notice;
 };
 
 export const getAllNotices = async () => {
-  const notices = await db
-    .collection('notices')
-    .find({})
-    .toArray();
+  const notices = await db.collection('notices').find({}).toArray();
 
   return notices;
 };
 
 export const getNoticesByCategory = async category => {
-  const notices = await db
-    .collection('notices')
-    .find({ category })
-    .toArray();
+  const notices = await db.collection('notices').find({category}).toArray();
 
   return notices;
 };
 
 export const getInbox = async () => {
-  const notices = await db
-    .collection('notices')
-    .find({ status: 'incoming' })
-    .toArray();
+  const notices =
+      await db.collection('notices').find({status : 'incoming'}).toArray();
   return notices;
 };
 
 export const getOutbox = async () => {
-  const notices = await db
-    .collection('notices')
-    .find({ status: 'outgoing' })
-    .toArray();
+  const notices =
+      await db.collection('notices').find({status : 'outgoing'}).toArray();
   return notices;
 };
 
 export const saveNotice = async input => {
-  logger.info('Savin the notices.', { service: 'API' });
+  logger.info('Savin the notices.', {service : 'API'});
   const count = await db.collection('notices').estimatedDocumentCount();
-  const doc = Object.assign(input, { _id: count + 1 });
+  const doc = Object.assign(input, {_id : count + 1});
   const notice = await db.collection('notices').insertOne(doc);
-  pubsub.publish('NEW_NOTICE', { newNotice: notice.ops[0] });
+  pubsub.publish('NEW_NOTICE', {newNotice : notice.ops[0]});
   return notice;
 };
